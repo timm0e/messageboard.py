f7922a4665d1c1052b62f3822b6778a1386c2532
timm0e <11885527+timm0e@users.noreply.github.com>
2019-03-24 12:58:16 +0100

    2019-03-24

--- messageboard/views.py
+++ messageboard/views.py
@@ -1,8 +1,12 @@
+import django.contrib.auth as auth
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User, UserManager
 from django.contrib.auth.views import LoginView
 from django.shortcuts import get_object_or_404
 # Create your views here.
+from django.utils.http import is_safe_url
 from django.views.generic import DetailView, TemplateView, FormView
 
 from messageboard.models import Board
@@ -41,4 +45,19 @@ class LoginSignupView(FormView):
     template_name = 'login.html'
     form_class = LoginForm
 
-    LoginView
+    def form_valid(self, form):
+        user = auth.authenticate(self.request, username=form.username, password=form.password)
+        if user is not None:
+            auth.login(self.request, user)
+            return super().form_valid()
+        if User.objects.filter(username=form.cleaned_data['username']).exists():
+            form.add_error('username', "User already exists")
+            form.add_error('password', "Wrong password")
+            return super().form_invalid(form)
+
+        user = UserManager.create_user(username=form., password=form.password)
+        auth.login(self.request, user)
+        messages.success(self.request, "New account created!")
+
+    def get_success_url(self):
+        return LoginView.get_success_url()
